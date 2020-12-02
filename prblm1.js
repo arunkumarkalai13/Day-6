40171d3c4c77c4be0da9daea9edd6fbbebebe5a3
arunkumarkalai13 <75129159+arunkumarkalai13@users.noreply.github.com>
2020-12-02 19:04:51 +0530

    Update prblm1.js

--- prblm1.js
+++ prblm1.js
@@ -1,7 +1,11 @@
 var sampleinput = [{name:"John",age:26},{name:"siva",age:27}];
 let arr = [];
+let befindobject = (sampleinput) => {
 for(let i=0;i<sampleinput.length;i++){
 arr += sampleinput[i].name;
 arr += ' ' ;
 }
-console.log(arr);
+  return arr;
+}
+
+console.log(befindobject(sampleinput));
