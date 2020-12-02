918c660a1cdbf5134b4b7a8e4de8454adafd39af
arunkumarkalai13 <75129159+arunkumarkalai13@users.noreply.github.com>
2020-12-02 19:01:26 +0530

    Add files via upload

---
+++ prblm1.js
@@ -0,0 +1,7 @@
+var sampleinput = [{name:"John",age:26},{name:"siva",age:27}];
+let arr = [];
+for(let i=0;i<sampleinput.length;i++){
+arr += sampleinput[i].name;
+arr += ' ' ;
+}
+console.log(arr);
